3177e5c0b889f70718e0cb33db1fc6b3adf502af
Hasibul <Hasibul@sumedha>
2017-09-20 09:17:37 +0530

    Changed object name

---
+++ first/first/hello.java
@@ -0,0 +1,17 @@
+package first;
+
+public class hello
+{
+ public	void Show()
+	{
+	 System.out.println("Hello World erterye");
+	}
+
+	public static void main(String[] args) 
+	{
+		hello obj1=new hello();
+		obj1.Show();
+
+	}
+
+}
