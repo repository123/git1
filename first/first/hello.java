84f1e3f7cebac5df7d90a3f22b4ce39fefc9f28b
repository123 <32119238+repository123@users.noreply.github.com>
2017-09-20 09:42:05 +0530

    Update hello.java

--- first/first/hello.java
+++ first/first/hello.java
@@ -6,10 +6,15 @@ public class hello
 	{
 	 System.out.println("Hello World erterye");
 	}
+	public void display()
+	{
+		System.out.println("IM in Display method!!");
+	}
 
 	public static void main(String[] args) 
 	{
 		hello obj1=new hello();
+		obj.display();
 		obj1.Show();
 
 	}
